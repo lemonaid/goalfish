c636cb972d49d0ff65744d9c01da074fd6510161
Jamie Duncan <jduncan@lemonaidstand.net>
2011-11-23 21:47:10 -0500

    cleanup commit

--- settings.py
+++ settings.py
@@ -17,6 +17,14 @@ You should have received a copy of the GNU General Public License
 along with Goalfish.es.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import os
+import django
+# calculated paths for django and the site
+# used as starting points for various other paths
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+
 ACTIVE_SCHOOL_YEAR='2012'
 
 #AUTHENTICATION_BACKENDS=('django.contrib.auth.backends.ModelBackend','Goalfish.backends.GoalFishAuth.MultiModelBackend',)
@@ -40,7 +48,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/jduncan/Documents/Aptana Studio 3 Workspace/Goalfish/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(SITE_ROOT, 'sqlite.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -71,19 +79,16 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
+MEDIA_URL = '/media'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
+
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_URL = '/static'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'zk554xk4ot$*dr2hiitkkie6tu(-f-8=q6rdrm4bz82$+ml#n0'
@@ -106,17 +111,13 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'Goalfish.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "/var/www/projects/goalfish/templates"
+                 os.path.join(SITE_ROOT,'static/templates')
 )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.admindocs',
@@ -125,5 +126,6 @@ INSTALLED_APPS = (
     'Goalfish.messaging',
     'Goalfish.notifications',
     'Goalfish.schools',
-    'Goalfish.academics'
+    'Goalfish.academics',
+    'Goalfish.django_extensions',
 )
